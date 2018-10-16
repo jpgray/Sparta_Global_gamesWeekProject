90a61d28903ef910a78519f6badad6f41b7ec7aa
Jon Gray <jgray@spartaglobal.com>
2018-10-16 11:26:20 +0100

    Full base game now playable, including end event and victory/defeat, next step to translate to html page and styling

--- javascript/js.js
+++ javascript/js.js
@@ -31,24 +31,16 @@ class encounter {
 class monster extends encounter{
   constructor(name, strength, charisma, speed, health, resilience, endurance, encounterChance, image, fightMessage, friendMessage, fleeMessage) {
     super (name, strength, charisma, speed, health, resilience, endurance, encounterChance, image, fightMessage, friendMessage, fleeMessage);
-    this.fight = fight;
-    ;
-     //fighting function here ^^
-    this.flee = () => {2
-    };
-      //flee function here ^^
-    this.friend = () => {3
-    };
-      //friend function here ^^
-    // this._defeat = () => "defeat special function"
+
   }
 }
 
 
+PName = prompt("Please enter a name for your daring adventurer...")
 
-
-const player = new encounter("Bob", 10, 10, 10, 100, 0, 0, 0);
-const playerR = new encounter("Bob", 10, 10, 10, 100, 0, 0, 0);
+const player = new encounter(PName, 10, 10, 10, 100, 0, 0, 0);
+const playerR = new encounter(PName, 10, 10, 10, 100, 0, 0, 0);
+const playerNew = new encounter(PName, 10, 10, 10, 100, 0, 0, 0);
 console.log(player);
 
 const Slime = new monster("Slime", 1, 1, 1, 30, 20, 3, 10, "");
@@ -63,11 +55,23 @@ const GiantRat = new monster("Giant Rat", 3, 3, 3, 30, 5, 20, 5, "");
 const GiantRatR = new monster("", 0, 0, 0, 30, 5, 20, 0, "");
 console.log(GiantRat);
 
-const TastyBanana = new monster("Tasty Banana", -100, 0, 0, 0.01, 100, 0, 2, "");
+const TastyBanana = new monster("Tasty Banana", -100, 0, 0, 0.01, 100, 0, 1, "");
 const TastyBananaR = new monster("", 0, 0, 0, 1, 100, 0, 0, "");
 TastyBanana.fiWinMessage = "You destroy what most assuredly was a villainous opponent plotting evil in a fit of rage against it's insulting attempt of temptation.";
-TastyBanana.frWinMessage = "You successfully negotiate the banana into your stomach. It is tasty."
+TastyBanana.friendMessage = "You successfully negotiate the banana into your stomach. It is tasty."
 console.log(TastyBanana);
+
+const Vampire = new monster("Vampire", 15, 8, 3, 50, 8, 20, 3, "");
+const VampireR = new monster("", 0, 0, 0, 50, 8, 20, 0, "");
+console.log(Vampire);
+
+const Goblin = new monster("Goblin", 9, 18, 14, 35, 8, 20, 4, "");
+const GoblinR = new monster("", 0, 0, 0, 35, 8, 20, 0, "");
+console.log(Goblin);
+
+const BBEG = new monster("BBEG", 25, 25, 25, 100, 100, 100, 0, "");
+const BBEGR = new monster("", 0, 0, 0, 35, 8, 20, 0, "");
+console.log(BBEG);
 
 const accumulator = (a, b) => a + b;
 const totalProb = probArray.reduce(accumulator,0);
@@ -75,7 +79,7 @@ const totalProb = probArray.reduce(accumulator,0);
 let kills = 0;
 let escapes = 0;
 let friends = 0;
-
+let totalVictories = () => kills + escapes + friends;
 
 
 
@@ -113,14 +117,24 @@ for (var i = 1; i < probArray.length; i++) {
 
 let currentEncounter;
 let setEncounter = () => {
+  console.log(totalVictories())
+  if (totalVictories() == 20) {
+    currentEncounter = BBEG;
+    console.log(currentEncounter);
+    return BBEG}
+    else if (totalVictories() == 21) {
+      console.log(`Congratulations! ${player.name} has won! Would you like to play again?`)
+    }
+  else{
   let ranVal = Math.random() * totalProb;
   console.log(`${ranVal} out of ${totalProb} was rolled!`);
   for (var i = 1; i < probAccArray.length; i++) {
     if (probAccArray[i-1] <= ranVal && ranVal <= probAccArray[i]) {
       currentEncounter = encounterArray[i];
       console.log(`A ${currentEncounter.name} was encountered!`)
+      console.log("What would you like to do? (Fight/Friend/Flee)")
       return encounterArray[i];
-      break;
+      }
     }
   }
 }
@@ -149,56 +163,90 @@ setEncounter();
 
 
 
-
+// let FIwin;
+// let FRwin;
+// let FLwin;
 
 
 // action effects
 
 fight = () => {
+  // FIwin = 0;
   playerFight = player.strength * ((Math.random())/2 +0.5);
-  console.log(playerFight);
+  console.log("player fight" + playerFight);
   currentEncounter.health -= playerFight;
-  console.log(currentEncounter.health)
+  console.log("enemy hp" + currentEncounter.health)
+  let ehp = currentEncounter.health
+
   instanceFiVictoryCheck(currentEncounter.health);
 
+  if (ehp > 0) {
+    console.log(currentEncounter.fightMessage);
+    enemyFight = currentEncounter.strength * ((Math.random())/2 +0.5);
+    console.log("enemy fight" + enemyFight);
+    player.health -= enemyFight;
+    console.log("player hp" + player.health);
 
-
-  if (currentEncounter.name == "Tasty Banana") {
-    console.log(TastyBanana.fiWinMessage)
-    currentEncounter.health--;
-    setEncounter();
-  }
+    if (currentEncounter.name == "Tasty Banana") {
+      console.log(TastyBanana.fiWinMessage);
+      currentEncounter.health--;
+      setEncounter();
+    }}
   deathCheck(player.health);
+
 }
 // currentEncounter.fight;
 
 friend = () => {
+  // FRwin = 0;
   playerFriend = player.charisma * ((Math.random())/2 +0.5);
   console.log(playerFriend);
   currentEncounter.resilience -= playerFriend;
   console.log(currentEncounter.resilience)
+  let res = currentEncounter.resilience
+
   instanceFrVictoryCheck(currentEncounter.resilience);
 
-
+  if (res > 0) {
+    console.log(currentEncounter.friendMessage);
+    enemyFight = currentEncounter.strength * ((Math.random())/2 +0.5);
+    console.log("enemy fight" + enemyFight);
+    player.health -= enemyFight;
+    console.log("player hp" + player.health);
 
   if (currentEncounter.name == "Tasty Banana") {
-    console.log(TastyBanana.frWinMessage)
     currentEncounter.health--;
     setEncounter();
-  }
+  }}
   deathCheck(player.health);
 
 }
 // currentEncounter.fight;
 
 flee = () => {
+  // FLwin = 0;
   playerFlee = player.speed * ((Math.random())/2 +0.5);
   console.log(playerFlee);
   currentEncounter.endurance -= playerFlee;
   console.log(currentEncounter.endurance)
+  let end = currentEncounter.endurance;
+
   instanceFlVictoryCheck(currentEncounter.endurance);
 
+  if (end > 0) {
+    console.log(currentEncounter.fleeMessage);
+    enemyFight = currentEncounter.strength * ((Math.random())/2 +0.5);
+    console.log("enemy fight" + enemyFight);
+    player.health -= enemyFight;
+    console.log("player hp" + player.health);
+
+  if (currentEncounter.name == "Tasty Banana") {
+    console.log(TastyBanana.frWinMessage)
+    currentEncounter.health--;
+    setEncounter();
+  }}
   deathCheck(player.health);
+
 }
 // currentEncounter.fight;
 
@@ -250,15 +298,17 @@ const instanceFiVictoryCheck = hp => {
   if (hp <= 0) {
     //create fight victory here
     console.log(`Congratulations! You killed the enemy ${currentEncounter.name}`)
-    if (currentEncounter.name != "Tasty Banana") {player.strength += currentEncounter.strength};
+    if (currentEncounter.name != "Tasty Banana") {player.strength += 0.5 * currentEncounter.strength};
     kills++;
     currentEncounter.health = encounterArray[currentEncounter.arrIndex+1].health;
     currentEncounter.resilience = encounterArray[currentEncounter.arrIndex+1].resilience;
     currentEncounter.endurance = encounterArray[currentEncounter.arrIndex+1].endurance;
     setEncounter();
+    FIwin = 1;
   }
   else {
     console.log(`The enemy ${currentEncounter.name} looks a bit shaken, but ready!`);
+    console.log("What would you like to do? (Fight/Friend/Flee)");
     // move to encounter's action
   }
 }
@@ -268,16 +318,18 @@ const instanceFrVictoryCheck = res => {
   if (res <= 0) {
     //create fight victory here, including + player skills
     console.log(`Congratulations! You have become buddies with the ${currentEncounter.name}!`)
-    player.charisma += currentEncounter.charisma;
+    player.charisma += 0.5 * currentEncounter.charisma;
     friends++;
     currentEncounter.health = encounterArray[currentEncounter.arrIndex+1].health;
     currentEncounter.resilience = encounterArray[currentEncounter.arrIndex+1].resilience;
     currentEncounter.endurance = encounterArray[currentEncounter.arrIndex+1].endurance;
     setEncounter();
+    FRwin = 1;
   }
   else {
     console.log(`The enemy ${currentEncounter.name} looks a bit conflicted, but remains angry!`);
-    // move to encounter's action
+    console.log("What would you like to do? (Fight/Friend/Flee)");
+    // moves to encounter's action
   }
 }
 // instanceFrVictoryCheck(currentEncounter.charisma)
@@ -286,15 +338,17 @@ const instanceFlVictoryCheck = end => {
   if (end <= 0) {
     //create fight victory here, including + player skills
     console.log(`Congratulations! You gallantly fled the enemy ${currentEncounter.name}!`)
-    player.speed += currentEncounter.speed;
+    player.speed += 0.5 * currentEncounter.speed;
     escapes++;
     currentEncounter.health = encounterArray[currentEncounter.arrIndex+1].health;
     currentEncounter.resilience = encounterArray[currentEncounter.arrIndex+1].resilience;
     currentEncounter.endurance = encounterArray[currentEncounter.arrIndex+1].endurance;
     setEncounter();
+    FLwin = 1;
   }
   else {
     console.log(`The enemy ${currentEncounter.name} looks a bit tired, but doggedly pursues you!`);
+    console.log("What would you like to do? (Fight/Friend/Flee)")
     // move to encounter's action
   }
 }
